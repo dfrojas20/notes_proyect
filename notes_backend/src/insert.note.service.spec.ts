cbd937e38d6c35b8a771b346e3e2d525db8b1b80
dfrojas20 <123292688+dfrojas20@users.noreply.github.com>
2024-06-28 01:58:44 -0400

    fixed unit testing backendf

--- notes_backend/src/insert.note.service.spec.ts
+++ notes_backend/src/insert.note.service.spec.ts
@@ -1,9 +1,19 @@
 
 import { insertNoteDto } from "./dto/insertNoteDto";
 import { INoteRepository } from "./repositories/I-note-repository";
+import { Note } from "./schemas/note.schema";
 import { InsertNoteService } from "./services/insert.note.service";
 
 class mockNoteRepository implements INoteRepository {
+    
+    async getAllNotes(): Promise<string | Note[]> {
+        let notes = [
+            new Note('titulo 1','contenido 1', new Date() ),
+            new Note('titulo 2','contenido 2', new Date() ),
+            new Note('titulo 3','contenido 3', new Date() ),
+           ]
+       return notes
+    }
     
     async crearNota(nota: insertNoteDto): Promise<string | insertNoteDto> {
         return nota
@@ -12,8 +22,8 @@ class mockNoteRepository implements INoteRepository {
 }
 
 
-describe('CrearNotaService', () => {
-    test('test_create_note', async () => {
+describe('test CrearNotaService', () => {
+    test('test creating a good note', async () => {
       //Arrange
       const insertNotaService = new InsertNoteService(new mockNoteRepository())
       const dto = new insertNoteDto('test note title', 'test note content') 
